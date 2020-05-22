5ae57db079f5f016a1b95b7f3b11d5300555e41b
Teresa Cho <tcho@cca.edu>
2020-05-21 17:14:54 -0700

    inital setup in config

--- config/site.config.js
+++ config/site.config.js
@@ -1,48 +1,13 @@
-const path = require('path');
-const fs = require('fs');
+# editorconfig.org
 
-let ROOT = process.env.PWD;
+root = true
 
-if (!ROOT) {
-  ROOT = process.cwd();
-}
+[*]
+charset = utf-8
+indent_size = 2
+indent_style = space
+insert_final_newline = true
+trim_trailing_whitespace = true
 
-const config = {
-  // Your website's name, used for favicon meta tags
-  site_name: 'Static Site Boilerplate',
-
-  // Your website's description, used for favicon meta tags
-  site_description: 'A modern boilerplate for static website development',
-
-  // Your website's URL, used for sitemap
-  site_url: '',
-
-  // Google Analytics tracking ID (leave blank to disable)
-  googleAnalyticsUA: '',
-
-  // The viewport meta tag added to your HTML page's <head> tag
-  viewport: 'width=device-width,initial-scale=1',
-
-  // Source file for favicon generation. 512x512px recommended.
-  favicon: path.join(ROOT, '/src/images/favicon.png'),
-
-  // Local development URL
-  dev_host: 'localhost',
-
-  // Local development port
-  port: process.env.PORT || 8000,
-
-  // Advanced configuration, edit with caution!
-  env: process.env.NODE_ENV,
-  root: ROOT,
-  paths: {
-    config: 'config',
-    src: 'src',
-    dist: 'dist',
-  },
-  package: JSON.parse(
-    fs.readFileSync(path.join(ROOT, '/package.json'), { encoding: 'utf-8' }),
-  ),
-};
-
-module.exports = config;
+[*.md]
+trim_trailing_whitespace = false
