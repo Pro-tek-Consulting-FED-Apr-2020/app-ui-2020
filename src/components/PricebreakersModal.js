8bbf9cfe2e617efbfea1f02097edcc1e17b29814
Teresa Cho <tcho@cca.edu>
2020-05-22 17:26:01 -0700

    creates boilerplate for modal

--- src/components/PricebreakersModal.js
+++ src/components/PricebreakersModal.js
@@ -5,22 +5,22 @@ class PriceBreakersModal extends React.Component {
   constructor(props) {
     super(props)
   } 
+  state = { show: false};
 
+  showModal = () => {
+    this.setState({ show: true});
+  };
 
-
-
-
-
-
-
-
+  hideModal = () => {
+    this.setState({show: false});
+  };
 
   render() {
     return(
       <div className='pricebreakers-container'>
         <div className='new-tag'>New</div>
           <div className='pricebreakers-header'>Introducing <span>Pricebreakers</span></div>
-        <button className='search-pricebreakers'>Search Pricebreakers</button>
+        <button type='button' className='search-pricebreakers'>Search Pricebreakers</button>
         <img src='/images/pricebreakers-img1.png' className='pricebreaker-img1' alt='hotel building icon'/>
         <img src='/images/pricebreakers-img2.png' alt='hotel building icon with thumbs up'/>
         <img src='/images/pricebreakers-img3.png' alt='door handle icon'/>
