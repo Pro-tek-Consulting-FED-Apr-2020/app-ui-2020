17cec878196a0b6788a69d3c481e73a3a888cd90
Teresa Cho <tcho@cca.edu>
2020-05-24 23:09:01 -0700

    adds in the sectioning of the container --> also includs future sizing for ad placement on righthand side

--- src/components/PricebreakersModal.js
+++ src/components/PricebreakersModal.js
@@ -17,17 +17,19 @@ class PriceBreakersModal extends React.Component {
 
   render() {
     return(
-      <div className='pricebreakers-container'>
-        <div className='new-tag'>New</div>
-          <div className='pricebreakers-header'>Introducing <span>Pricebreakers</span></div>
-        <button type='button' className='search-pricebreakers'>Search Pricebreakers</button>
-        <div className="three-pricebreaker-icon">
-          <img src='/images/pricebreakers-img1.png' className='img1' alt='hotel building icon'/>
-          <img src='/images/pricebreakers-img2.png' alt='hotel building icon with thumbs up'/>
-          <img src='/images/pricebreakers-img3.png' alt='door handle icon'/>
-        </div>
-
-          
+      <div className='pricebreakers-section'>
+        <div className='pricebreakers-container'>
+          <div className='pricebreakers-box'>
+            <div className='new-tag'>New</div>
+              <div className='pricebreakers-header'>Introducing <span>Pricebreakers</span></div>
+            <button type='button' className='search-pricebreakers'>Search Pricebreakers</button>
+            <div className="three-pricebreaker-icon">
+              <img src='/images/pricebreakers-img1.png' className='img1' alt='hotel building icon'/>
+              <img src='/images/pricebreakers-img2.png' alt='hotel building icon with thumbs up'/>
+              <img src='/images/pricebreakers-img3.png' alt='door handle icon'/>
+            </div>
+          </div>
+        </div>          
       </div>
     )
   }
